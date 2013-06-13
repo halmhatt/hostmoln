6fe77ff4f991fb61aae8b54fce9dcbe205bb7bfb
Jacob Carlsson <jacob.carlsson@gmail.com>
2013-06-13 22:06:50 +0200

    Add som comments

--- html/html.py
+++ html/html.py
@@ -1,26 +1,32 @@
 
 class Tag():
+	"""Simple tag class to hold a html tag"""
 
 	tagname = 'div'
 	attributes = []
 	content = ''
 	
 	def __init__(self, tagname='div', content='', attributes={}):
+	
 		self.tagname = tagname
 		self.content = content
 		self.attributes = attributes
 	
 	def __str__(self):
+		"""Convert to string"""
 	
 		# If closing itself
 		if(self.content is None):
 			return '<{0} {1}/>'.format(self.tagname, self.attr())
 	
-		return '<{0} {1}>{2}</{0}>'.format(self.tagname, self.attr(), self.content)
+		return '<{0} {1}>{2}</{0}>'.format(self.tagname, self.formatAttributes(), self.content)
 			
-	def attr(self):
+	def formatAttributes(self):
+		"""Ouput all attributes"""
+		
 		attributes = []
 		
+		# Format each attribute
 		for key in self.attributes:
 			attributes.append(key + '="' + self.attributes[key] + '"')
 	
