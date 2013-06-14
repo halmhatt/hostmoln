68846d287d1d1e009473fc2fbe58431561098564
Jacob Carlsson <jacob.carlsson@gmail.com>
2013-06-14 17:29:10 +0200

    Namechange

--- lang/install.sh
+++ lang/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cp /home/jacob/git/jacob/python/simple-markup/lang/simplemarkup.lang /usr/share/gtksourceview-3.0/language-specs/simplemarkup.lang
+cp /home/jacob/git/jacob/python/simple-markup/lang/hostmoln.lang /usr/share/gtksourceview-3.0/language-specs/hostmoln.lang
